dd2e832b03c71098e4062946dc32bab2ee749d52
GitHub Action <action@github.com>
2023-08-30 22:23:15 +0000

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='celery-smartbase',
-    version='0.3.4',
+    version='0.3.5',
     packages=find_packages(),
     install_requires=[
         'Django~=3.2.11',
